055946d01f6289273f48be9f82ebfa56a98cbb6a
w.jamesdevera <w.jamesdevera@gmail.com>
2026-08-04 06:41:08 +0800

    refactor: add models directory

--- src/fintrack/models/models.py
+++ src/fintrack/models/models.py
@@ -1,38 +1,6 @@
-from typing import Optional
-import uuid
 from decimal import Decimal
-from datetime import datetime, timezone
-from time import timezone
-from sqlmodel import Field, SQLModel, Relationship
-
-
-class SimpleIDModel(SQLModel):
-    id: int | None = Field(primary_key=True)
-
-
-class UUIDIDModel(SQLModel):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-
-
-class TimestampModel(SQLModel):
-    created_at: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
-
-    )
-    updated_at: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc),
-        sa_column_kwargs={
-            "onupdate": lambda:
-                datetime.now(timezone.utc)
-        }
-    )
-
-
-class EventTimestampModel(SQLModel):
-    occured_at: Optional[datetime] = Field(
-        default_factory=lambda: datetime.now(timezone.utc)
-
-    )
+from sqlmodel import Field, Relationship
+from .core import SimpleIDModel, UUIDIDModel, TimestampModel
 
 
 class Category(SimpleIDModel, table=True):
