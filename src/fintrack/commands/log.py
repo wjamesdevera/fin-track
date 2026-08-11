4f39af298ae144ac69d976cd84c026fcc4796dea
w.jamesdevera <w.jamesdevera@gmail.com>
2026-08-12 00:22:55 +0800

    feat: log command add transaction to db

--- src/fintrack/commands/log.py
+++ src/fintrack/commands/log.py
@@ -1,6 +1,6 @@
 import click
-from sqlmodel import Session
-from ..models.models import Transaction
+from sqlmodel import Session, select
+from ..models.models import Transaction, SubCategory, TransactionType
 from ..db import engine
 
 
@@ -10,6 +10,21 @@ from ..db import engine
 @click.option("-c", "--category", type=str)
 @click.option("-n", "--note", type=str, default=None)
 def log(amount: float, type: str, category: str, note: str | None) -> None:
+    with Session(engine) as session:
+        statement = select(SubCategory).where(
+            SubCategory.name == category)
+        obj_category = session.exec(statement).first()
 
-    click.echo(
-        f'amount: {amount} | type: {type} | category: {category} | note: {note}')
+        if not obj_category:
+            raise ValueError("Category not found")
+
+        new_type = TransactionType.EXPENSE if type.lower(
+        ) == "expense" else TransactionType.INCOME
+
+        new_transaction = Transaction(
+            amount=amount, type=new_type, sub_category_id=obj_category.id, note=note)
+
+        session.add(new_transaction)
+        session.commit()
+        click.echo(
+            f'amount: ${amount} | type: {new_type} | category: {obj_category.name} | note: {note}')
