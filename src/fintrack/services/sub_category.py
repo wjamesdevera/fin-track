46155300452e9e0d8cf0782a1a349824444782be
w.jamesdevera <w.jamesdevera@gmail.com>
2026-08-14 10:20:48 +0800

    feat: add validation for adding duplicated sub category

--- src/fintrack/services/sub_category.py
+++ src/fintrack/services/sub_category.py
@@ -2,7 +2,15 @@ from fintrack.models.category import SubCategory, Category
 from sqlmodel import Session
 
 
+class SubCategoryExist(Exception):
+    pass
+
+
 def add_sub_category(category: Category, name: str, session: Session) -> SubCategory:
+    for sc in category.sub_categories:
+        if sc.name == name:
+            raise SubCategoryExist
+
     new_sc = SubCategory(name=name, category=category)
     session.add(new_sc)
     session.commit()
